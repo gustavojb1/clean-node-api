bd8c038dbc5ea820d56d0f8d717c387d85fe72b5
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-03-04 14:37:41 -0300

    chore: jest and package config changes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    roots: ["<rootDir>/tests"],
+    roots: ["<rootDir>/src"],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts'
+      ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
     transform: {
     "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    }
+    // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 }
